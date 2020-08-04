fc73af020922d60e8a1f453a48f0f13a7f22c7cf
Dima Tiberiu <dima.tiberiu1@gmail.com>
2020-08-05 00:45:12 +0300

    Added Entity framework + Database SQL. Modified container to use the new SqlRestaurantData dependency injection for IRestaurantData

---
+++ Restaurant.Data/Services/RestaurantsChainDbContext.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantChain.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantChain.Data.Services
+{
+   public class RestaurantsChainDbContext : DbContext
+   {
+      public DbSet<Restaurant> Restaurants { get; set; }
+   }
+}
---
+++ Restaurant.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantChain.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantChain.Data.Services
+{
+   public class SqlRestaurantData : IRestaurantData
+   {
+      private readonly RestaurantsChainDbContext db;
+
+      public SqlRestaurantData(RestaurantsChainDbContext db)
+      {
+         this.db = db;
+      }
+
+      public void AddRestaurant(Restaurant restaurant)
+      {
+         db.Restaurants.Add(restaurant);
+         db.SaveChanges();
+      }
+
+      public void EditRestaurant(Restaurant newRestaurant)
+      {
+         var entry = db.Entry(newRestaurant);
+         entry.State = EntityState.Modified;
+         db.SaveChanges();
+      }
+
+      public IEnumerable<Restaurant> GetAllRestaurants()
+      {
+         return from r in db.Restaurants
+                orderby r.Name
+                select r;
+      }
+
+      public Restaurant GetRestaurantForId(int id)
+      {
+         return db.Restaurants.FirstOrDefault(r => r.Id == id);
+      }
+   }
+}
